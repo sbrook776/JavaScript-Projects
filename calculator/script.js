ddebd52b8d56b65ad3bd31852ff90ad7e3eb7512
Sean Brook <sbrook776@gmail.com>
2024-01-17 11:23:51 +1300

    calculator complete

--- calculator/script.js
+++ calculator/script.js
@@ -2,6 +2,19 @@ const calculatorDisplay = document.querySelector("h1");
 const inputBtns = document.querySelectorAll("button");
 const clearBtn = document.getElementById("clear-btn");
 
+// Calculate first and second values depending on operator
+const calculate = {
+	"/": (firstNumber, secondNumber) => firstNumber / secondNumber,
+
+	"*": (firstNumber, secondNumber) => firstNumber * secondNumber,
+
+	"+": (firstNumber, secondNumber) => firstNumber + secondNumber,
+
+	"-": (firstNumber, secondNumber) => firstNumber - secondNumber,
+
+	"=": (firstNumber, secondNumber) => secondNumber,
+};
+
 let firstValue = 0;
 let operatorValue = "";
 let awaitingNextValue = false;
@@ -31,12 +44,19 @@ function addDecimal() {
 function useOperator(operator) {
 	const currentValue = Number(calculatorDisplay.textContent);
 	// Prevent multiple operators
-	if (operatorValue && awaitingNextValue) return;
+	if (operatorValue && awaitingNextValue) {
+		operatorValue = operator;
+		return;
+	}
 	// Assign firstValue if no value
 	if (!firstValue) {
 		firstValue = currentValue;
 	} else {
-		console.log(firstValue, operatorValue, currentValue);
+		// console.log(firstValue, operatorValue, currentValue);
+		const calculation = calculate[operatorValue](firstValue, currentValue);
+		// console.log('calculation', calculation);
+		calculatorDisplay.textContent = calculation;
+		firstValue = calculation;
 	}
 	// Ready for next value, store operator
 	awaitingNextValue = true;
